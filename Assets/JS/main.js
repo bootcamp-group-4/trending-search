65d50907562b7a78c2b3ad7711cde3e8c1660304
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-09-28 19:03:52 -0700

    Added base API work

---
+++ Assets/JS/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+//website address we get
+// create app on twitter to get twitter api keys (4 keys)
+var newsUrl = "https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=ef7abdb0170d49058f8f1efdb483f219";
+//var newsApi = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=8c03397f019f4c738c663b53434095fc"
+
+
+$.ajax({
+  url: newsUrl,
+  method: "GET",
+
+}).done(function(response) {
+   console.log(response.articles[0].title);
+
+$("#one").on("click", function() {
+  var result = response;
+  console.log(result);
+    for(var i=0; i < result.articles.length; i++){
+  console.log(result.articles[i].title);
+  }
+
+console.log("here");
+
+
+
+
+});
+});
+});
