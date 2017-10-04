10a6397e32c262628b7d3e196813b30d6c53dadb
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-10-03 22:08:52 -0700

    Added the animate on scroll library

--- Assets/JS/image.js
+++ Assets/JS/image.js
@@ -1,4 +1,3 @@
-// $(document).ready(function(){
 
 
 function callApi(searhStringParam) {
@@ -14,39 +13,32 @@ function callApi(searhStringParam) {
 	}).done(function(response){
 
 		console.log(response);
-			// console.log("image!");
-			// console.log(result);
-		// for(i = 0; i < result.image[0].length; i++);
 
-		//1.grab response and save it to a var
 		var image = response;
 		grabImage(image);
-		//2.call a function with the declared var
-			//look below
-		//3.make a for loop for the returned values
 
-		//4.construct the image DOM element and assign src value then append.
-
-
-
-
-			// document.getelementbyid("trend-container");
 	});
-};
+}
 
 
 function grabImage(response) {
 	for(i = 0; i < response.images.length; i++){
-		var int = $("<img>"); //creates an image dom eleement
-		console.log(int);
-		console.log(response.images[i].display_sizes[0].uri);
-		int.attr({
-			src: response.images[i].display_sizes[0].uri
-
+		var img = $("<img>"); //creates an image dom eleement
+		img.attr({
+			src: response.images[i].display_sizes[0].uri,
+			'data-aos': 'fade-up',
+			id: i
 		});
-		writeToDom(int);
+		writeToDom(img);
 	};
-};
+	setImageAnimateOffset();
+}
+
+function setImageAnimateOffset() {
+	setTimeout(function() {
+		AOS.refreshHard();
+	}, 500)
+}
 
 function writeToDom(imageElement) {
 	$("#result-container").append(imageElement);
